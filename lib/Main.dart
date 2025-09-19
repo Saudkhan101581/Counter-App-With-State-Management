009bde10964b4bc9eb9afd315b34786d5e4fc9ae
Saudkhan101581 <saudkhan17218@gmail.com>
2025-09-19 17:12:47 +0500

    Creating Counter UI with State Management

---
+++ lib/Main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class MainDart extends StatefulWidget {
+  const MainDart({super.key});
+
+  @override
+  State<MainDart> createState() => _MainDartState();
+}
+
+class _MainDartState extends State<MainDart> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ,
+    );
+  }
+}
---
+++ lib/Views/Counter_UI.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class CounterUi extends StatefulWidget {
+  const CounterUi({super.key});
+
+  @override
+  State<CounterUi> createState() => _CounterUiState();
+}
+
+class _CounterUiState extends State<CounterUi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffFFFFFF),
+
+    );
+  }
+}
